64319d3befab6f5530c4fe20931a5c67e4681de4
divya_mishra <divyashalumishrabua@gmail.com>
2022-06-28 16:44:00 -0500

    code updated

--- Frontend/src/app/home/dashboard/dashboard.component.ts
+++ Frontend/src/app/home/dashboard/dashboard.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartDataSets, ChartOptions } from 'chart.js';
-import { Color, Label } from 'ng2-charts';
-  
+import { Chart } from 'chart.js';
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  public lineChartData: ChartDataSets[] = [
-    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  public lineChartOptions: (ChartOptions & { annotation ?: any }) = {
-    responsive: true,
-  };
-  public lineChartColors: Color[] = [
-    {
-      borderColor: 'black',
-      backgroundColor: 'rgba(255,0,0,0.3)',
-    },
-  ];
-  public lineChartLegend = true;
-  public lineChartType = 'line';
-  public lineChartPlugins = [];
-
   constructor() { }
-
   ngOnInit(): void {
+    var myChart = new Chart("myChart1", {
+      type: 'line',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 10, 7, 5, 2, 3],
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 159, 64, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              max: 15,
+              stepSize: 2,
+              precision: 0
+            }
+          }]
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
   }
 
 }
